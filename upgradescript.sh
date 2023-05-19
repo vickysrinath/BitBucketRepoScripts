4c3e17759d6fbf5a25761840617ce7d266715ab7
vickysrinath <49038804+vickysrinath@users.noreply.github.com>
2023-05-19 12:42:35 +0530

    Update upgradescript.sh

--- upgradescript.sh
+++ upgradescript.sh
@@ -7,30 +7,27 @@ if [[ "$node_type" == "1" ]]; then
     read -p "Enter the group name: " group_name
 fi
 
-file_name="maintenance.json"
-directory_path="/apigw/v770/apigateway"
-file_path="${directory_path}/${file_name}"
+apigw_directory="/apigw/v770/apigateway"
+maintenance_file_path="${apigw_directory}/maintenance.json"
 content="Content Here"
 
-# Create the directory if it doesn't exist
-mkdir -p "$directory_path"
 
-# Create the file and write the content
-echo "$content" > "$file_path"
+# Create the maintanience file and write the content
+echo "$content" > "$maintenance_file_path"
 
-if [ -e "$file_path" ]; then
+if [ -e "$maintenance_file_path" ]; then
     echo "Maintenance file created successfully."
 else
     echo "An error occurred while creating the maintenance file. Exiting..."
     exit 1
 fi
 
-# Copy the folder with timestamp
+# Backup the current verison
 backup_directory="/apigw/v770_Feb22_backup_$(date +%Y-%m-%d_%H-%M)"
 cp -R "/apigw/v770/" "$backup_directory"
 
 if [ -d "$backup_directory" ]; then
-    echo "Folder copied to $backup_directory successfully."
+    echo "Backup to directory $backup_directory is successful."
 else
     echo "An error occurred while copying the folder to $backup_directory. Exiting..."
     exit 1
@@ -50,7 +47,7 @@ else
     exit 1
 fi
 
-# Execute nodemanager and startinstance with -k option
+# Kill the Exisitng Process
 if [[ "$node_type" == "1" ]]; then
     /apigw/v770/apigateway/posix/bin/startinstance -n "$instance_name" -g "$group_name" -k
 fi
@@ -60,23 +57,38 @@ fi
 # Check getcap status
 getcap_output=$(getcap /apigw/v770/apigateway/platform/bin/vshell)
 if [ -n "$getcap_output" ]; then
-    echo "getcap output:"
-    echo "$getcap_output"
+    echo "setcap is enabled."
     setcap_check=true
 else
     setcap_check=false
     echo "setcap is not enabled."
 fi
 
-# Remove setcap if getcap status is true
+# Remove setcap if setcap status is true
 if $setcap_check; then
     dzdo su -
     setcap -r /apigw/v770/apigateway/platform/bin/vshell
     echo "setcap removed for /apigw/v770/apigateway/platform/bin/vshell"
-    exit  # Logout from root after setcap removal
+    exit  
 fi
 
 # Upgrade Script
-/apigw/77update_Feb23/update_apigw.sh --install_dir /apigw/v770/ --no backup --mode unattended
+upgrade_command_output=$(/apigw/77update_Feb23/update_apigw.sh --install_dir /apigw/v770/ --no backup --mode unattended 2>&1)
+if echo "$upgrade_command_output" | grep -q "System update completed$"; then
+    echo "Upgrade done."
+else
+    echo "An error occurred during the upgrade. Please check the logs for more details."
+fi
 
+if $setcap_check; then
+    dzdo su -
+    setcap 'cap_net_bind_service=+ep /apigw/v770/apigateway/platform/bin/vshell'
+    echo "setcap enabled for /apigw/v770/apigateway/platform/bin/vshell"
+    exit  
+fi
 
+#start process
+/apigw/v770/apigateway/posix/bin/nodemanager -d
+if [[ "$node_type" == "1" ]]; then
+    /apigw/v770/apigateway/posix/bin/startinstance -n "$instance_name" -g "$group_name" -d
+fi
